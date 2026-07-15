18f3dd958164727fe9ebce93e210997e97d439fd
Purushoth6357 <purushoth6357@gmail.com>
2026-07-15 20:11:44 +0530

    Full Adder Behavioural modelling changes

--- FullAdder/fulladder_behavioural.v
+++ FullAdder/fulladder_behavioural.v
@@ -4,8 +4,17 @@ module FullAdder_Behavioural(
 );
 
 always @(*) begin
-    S    = A ^ B ^ Cin;
-    Cout = (A & B) | (Cin & (A ^ B));
+    case ({A, B, Cin})
+        3'b000: {S, Cout} = 2'b00;
+        3'b001: {S, Cout} = 2'b10;
+        3'b010: {S, Cout} = 2'b10;
+        3'b011: {S, Cout} = 2'b01;
+        3'b100: {S, Cout} = 2'b10;
+        3'b101: {S, Cout} = 2'b01;
+        3'b110: {S, Cout} = 2'b01;
+        3'b111: {S, Cout} = 2'b11;
+        default: {S, Cout} = 2'bxx;
+    endcase
 end
 
 endmodule
