266e16ce299b112873ba33023d03d31a1d2b7ceb
Purushoth6357 <purushoth6357@gmail.com>
2026-07-22 18:52:21 +0530

    Build a 4 bit CLA

---
+++ CLA/CLA8b.v
@@ -0,0 +1,10 @@
+module CLA8b (
+    input [7:0] A, B,
+    input Cin,
+    output [7:0] S,
+    output Cout
+);
+    wire C1;
+    CLA4b cla1(.A(A[3:0]), .B(B[3:0]), .Cin(Cin), .S(S[3:0]), .Cout(C1));
+    CLA4b cla2(.A(A[7:4]), .B(B[7:4]), .Cin(C1), .S(S[7:4]), .Cout(Cout));
+endmodule
